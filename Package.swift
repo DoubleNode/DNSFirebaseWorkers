9750aa991d805653cf38df0f955c3f7c3c9e6d0a
Darren Ehlers <me@darrenehlers.com>
2022-09-04 20:38:43 -0500

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,54 @@
+// swift-tools-version:5.7
+//
+//  Package.swift
+//  DoubleNode Swift Framework (DNSFramework) - DNSFirebaseWorkers
+//
+//  Created by Darren Ehlers.
+//  Copyright © 2022 - 2016 DoubleNode.com. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "DNSFirebaseWorkers",
+    platforms: [
+        .iOS(.v13),
+        .tvOS(.v13),
+        .macOS(.v10_15),
+        .watchOS(.v6),
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "DNSFirebaseWorkers",
+            type: .static,
+            targets: ["DNSFirebaseWorkers"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/DoubleNodeOpen/AtomicSwift.git", from: "1.2.2"),
+        .package(url: "https://github.com/DoubleNode/DNSBlankWorkers.git", from: "1.9.60"),
+        .package(url: "https://github.com/DoubleNode/DNSCore.git", from: "1.9.32"),
+        .package(url: "https://github.com/DoubleNode/DNSCoreThreading.git", from: "1.9.0"),
+        .package(url: "https://github.com/DoubleNode/DNSDataObjects.git", from: "1.9.36"),
+        .package(url: "https://github.com/DoubleNode/DNSError.git", from: "1.9.2"),
+        .package(url: "https://github.com/DoubleNode/DNSProtocols.git", from: "1.9.85"),
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "9.3.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "DNSFirebaseWorkers",
+            dependencies: [
+                "AtomicSwift", "DNSBlankWorkers", "DNSCore", "DNSCoreThreading",
+                "DNSDataObjects", "DNSError", "DNSProtocols",
+                .product(name: "FirebaseAnalytics", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
+            ]),
+        .testTarget(
+            name: "DNSFirebaseWorkersTests",
+            dependencies: ["DNSFirebaseWorkers"]),
+    ],
+    swiftLanguageVersions: [.v5]
+)
---
+++ README.md
@@ -0,0 +1,60 @@
+![DoubleNode Swift Framework](https://github.com/DoubleNode/DNSCore/raw/master/DNSFrameworkLogo.png)
+
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/6f473642e4404426b55fda500602e662)](https://www.codacy.com?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=DoubleNode/DNSFirebaseWorkers&amp;utm_campaign=Badge_Grade)
+[![codecov](https://codecov.io/gh/DoubleNode/DNSCoreValidationWorker/branch/master/graph/badge.svg?token=NcFMBk0g9t)](https://codecov.io/gh/DoubleNode/DNSFirebaseWorkers)
+
+# DNSFirebaseWorkers
+
+DNSFirebaseWorkers is a collection of Firebase Worker classes used for the DNSFramework
+
+-   [Features](#features)
+-   [Requirements](#requirements)
+-   [Installation](#installation)
+-   [Dependencies](#dependencies)
+-   [Credits](#credits)
+-   [License](#license)
+
+## Features
+
+-   [x] It's just awesome!
+
+## Requirements
+
+-   iOS 13.0+ / macOS 10.15+ / tvOS 13.0+ / watchOS 6.0+
+-   Xcode 11.6+
+-   Swift 5.1+
+
+## Installation
+
+### Swift Package Manager
+
+The [Swift Package Manager](https://swift.org/package-manager/) is a tool for automating the distribution of Swift code and is integrated into the `swift` compiler. It is in early development, but Alamofire does support its use on supported platforms.
+
+Once you have your Swift package set up, adding Alamofire as a dependency is as easy as adding it to the `dependencies` value of your `Package.swift`.
+
+```swift
+dependencies: [
+.package(url: "https://github.com/DoubleNode/DNSFirebaseWorkers.git", from: "1.9.2")
+]
+```
+
+## Dependencies
+
+-   AtomicSwift v1.2.2+
+-   DNSBlankWorkers v1.9.60+
+-   DNSCore v1.9.32+
+-   DNSCoreThreading v1.9.0+
+-   DNSDataObjects v1.9.36+
+-   DNSError v1.9.2+
+-   DNSProtocols v1.9.85+
+-   FirebaseAnalytics v9.3.0+
+-   FirebaseFirestore v9.3.0+
+
+
+## Credits
+
+DNSCoreValidationWorker and the DNSFramework are owned and maintained by Darren Ehlers and [DoubleNode.com](http://doublenode.com). You can follow them on Twitter at [@DoubleNodeOpen](https://twitter.com/DoubleNodeOpen) for project updates and releases.
+
+## License
+
+DNSCoreValidationWorker and the DNSFramework are Copyright © 2020 - 2016 Darren Ehlers and DoubleNode, LLC. All rights reserved.
